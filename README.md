b6acb5d27a391168ae837b7c2e981d6cb46e90b3
Ralf Thielow <ralf.thielow@gmail.com>
2013-08-14 09:59:02 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 pom-merge-driver
 ================
 
-Custom merge driver for Maven's pom.xml.
+Custom Git merge driver for Maven's pom.xml.
+
+If there was a change in the project version in a
+branch to be merged (while this change was not merged
+yet), use their version to avoid conflicts and be
+consistent in case of trivial merges that might happen
+at the same time. All other changes in the file will
+be merged as usual.
+
+Installation
+-------------
+
+git-config:
+<pre>
+[merge "pommerge"]
+        name = A custom merge driver for Maven's pom.xml
+        driver = $PATH_TO/mergepom.py %O %A %B
+</pre>
+.gitattributes
+<pre>
+pom.xml merge=pommerge
+</pre>
---
+++ mergepom.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python2
+
+# Copyright 2013 Ralf Thielow <ralf.thielow@gmail.com>
+# Licensed under the GNU GPL version 2.
+
+import sys, subprocess, shlex
+import xml.dom.minidom as dom
+
+def change_version(old_version, new_version, cont):
+	return cont.replace("<version>"+old_version+"</version>",
+			"<version>" + new_version + "</version>")
+
+def get_project_version(f):
+	try:
+		tree = dom.parse(f)
+		for entry in tree.documentElement.childNodes:
+			if entry.nodeName == "version":
+				return entry.firstChild.data
+			if entry.nodeName == "parent":
+				for entry2 in entry.childNodes:
+					if entry2.nodeName == 'version':
+						return entry2.firstChild.data
+		return 'unknown'
+	except:
+		print sys.argv[0] + ': error while parsing pom.xml'
+		return 'unknown'
+
+if len(sys.argv) < 4 or len(sys.argv) > 5:
+	print "Wrong number of arguments."
+	sys.exit(-1)
+
+ancestor_version = get_project_version(sys.argv[1])
+current_branch_version = get_project_version(sys.argv[2])
+other_branch_version = get_project_version(sys.argv[3])
+
+if (current_branch_version != 'unknown' and
+		other_branch_version != 'unknown' and
+		ancestor_version != 'unknown' and
+		current_branch_version != other_branch_version and
+		other_branch_version != ancestor_version):
+	f = open(sys.argv[2],'r')
+	other = f.read()
+	f.close()
+	other = change_version(current_branch_version, other_branch_version, other)
+	f = open(sys.argv[2],'w')
+	f.write(other)
+	f.close()
+
+cmd = "git merge-file -p -L mine -L base -L theirs " + sys.argv[2] + " " + sys.argv[1] + " " + sys.argv[3]
+p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
+git_merge_res = p.communicate()[0]
+ret = p.returncode
+
+f = open(sys.argv[2],'w')
+f.write(git_merge_res)
+f.close
+
+sys.exit(ret)
