560e22f62a911d2aec7bea4e5d705d714b507213
Ralf Thielow <ralf.thielow@gmail.com>
2020-04-27 07:29:47 +0200

    use python3, retab file

--- mergepom.py
+++ mergepom.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 # Copyright 2013 Ralf Thielow <ralf.thielow@gmail.com>
 # Licensed under the GNU GPL version 2.
@@ -25,40 +25,40 @@ def get_tag(f):
                         return matchingNodes.firstChild.nodeValue
                 return None
         except Exception as e:
-                print e
+                print(e)
                 print(sys.argv[0] + ': error while parsing pom.xml')
                 return None
 
 def change_version(old_version, new_version, cont):
-	return cont.replace("<version>"+old_version+"</version>",
-			    "<version>" + new_version + "</version>")
+        return cont.replace("<version>"+old_version+"</version>",
+                            "<version>" + new_version + "</version>")
 
 def get_project_version(f):
-	try:
-		tree = dom.parse(f)
-		version = None
-		parent_version = None
-		for entry in tree.documentElement.childNodes:
-			if entry.nodeName == "version":
-				version = entry.firstChild.data
-			if entry.nodeName == "parent":
-				for entry2 in entry.childNodes:
-					if entry2.nodeName == 'version':
-						parent_version = entry2.firstChild.data
+        try:
+                tree = dom.parse(f)
+                version = None
+                parent_version = None
+                for entry in tree.documentElement.childNodes:
+                        if entry.nodeName == "version":
+                                version = entry.firstChild.data
+                        if entry.nodeName == "parent":
+                                for entry2 in entry.childNodes:
+                                        if entry2.nodeName == 'version':
+                                                parent_version = entry2.firstChild.data
 
-		if version is not None:
-			# version has a priority over parent version
-			return version
-		else:
-			# may return None
-			return parent_version
-	except:
-		print(sys.argv[0] + ': error while parsing pom.xml')
-		return None
+                if version is not None:
+                        # version has a priority over parent version
+                        return version
+                else:
+                        # may return None
+                        return parent_version
+        except:
+                print(sys.argv[0] + ': error while parsing pom.xml')
+                return None
 
 if len(sys.argv) < 4 or len(sys.argv) > 5:
-	print("Wrong number of arguments.")
-	sys.exit(-1)
+        print("Wrong number of arguments.")
+        sys.exit(-1)
 
 ancestor_version = get_project_version(sys.argv[1])
 current_branch_version = get_project_version(sys.argv[2])
@@ -70,20 +70,20 @@ have_tags = True if current_tag is not None and other_tag is not None else False
 
 # change current version in order to avoid merge conflicts
 if (
-	current_branch_version is not None
-	and other_branch_version is not None
-	and ancestor_version is not None
-	and current_branch_version != other_branch_version
-	and other_branch_version != ancestor_version
+        current_branch_version is not None
+        and other_branch_version is not None
+        and ancestor_version is not None
+        and current_branch_version != other_branch_version
+        and other_branch_version != ancestor_version
 ):
-	with open(sys.argv[2], 'r') as f:
-		enc = get_enc(f.readline(), 'utf-8')
-	with codecs.open(sys.argv[2], 'r', enc) as f:
-		other = f.read()
-	other = change_version(current_branch_version, other_branch_version, other)
+        with open(sys.argv[2], 'r') as f:
+                enc = get_enc(f.readline(), 'utf-8')
+        with codecs.open(sys.argv[2], 'r', enc) as f:
+                other = f.read()
+        other = change_version(current_branch_version, other_branch_version, other)
         other = change_tag(current_tag, other_tag, other) if have_tags else other
-	with codecs.open(sys.argv[2], 'w', enc) as f:
-		f.write(other)
+        with codecs.open(sys.argv[2], 'w', enc) as f:
+                f.write(other)
 
 cmd = "git merge-file -p -L mine -L base -L theirs " + sys.argv[2] + " " + sys.argv[1] + " " + sys.argv[3]
 p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
@@ -114,15 +114,15 @@ val = val.strip().decode('utf-8')
 
 keep = False
 if (p.returncode == 0 and val == 'true'):
-	keep = True
+        keep = True
 
 # revert pom project version on current branch, unless in master. Allows for gitflow release-finish, hotfix-finish, and feature-finish to work better
 if (current_branch_version is not None and (keep or branch != 'master')):
-	print('Merging pom version ' + other_branch_version + ' into ' + branch + '. Keeping version ' + current_branch_version)
-	git_merge_res_str = change_version(other_branch_version, current_branch_version, git_merge_res_str)
+        print('Merging pom version ' + other_branch_version + ' into ' + branch + '. Keeping version ' + current_branch_version)
+        git_merge_res_str = change_version(other_branch_version, current_branch_version, git_merge_res_str)
         git_merge_res_str = change_tag(other_tag, current_tag, git_merge_res_str) if have_tags else git_merge_res_str
 
 with codecs.open(sys.argv[2], 'w', enc) as f:
-	f.write(git_merge_res_str)
+        f.write(git_merge_res_str)
 
 sys.exit(ret)
